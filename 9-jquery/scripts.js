49737716006bdd8f4e1df8c4da7f0d40056d7fb9
Christopher Mattox <christophermattox@Christophers-MacBook-Pro.local>
2016-10-31 16:05:47 -0500

    Noticed some problems

--- 9-jquery/scripts.js
+++ 9-jquery/scripts.js
@@ -1,7 +1,7 @@
 	// 1 - After the DOM loads, add a paragraph tag that says "Page loaded"
 	// to the section element
-$(document).ready(function() {
-  function appendDom() {
+jQuery(document).ready(function() {
+
         $('#section').append('<p>Page Loaded</p>');
 	// 2 - Change the text of the h1 element to something (anything!) else.
         $('h1').append('<h1>WHY IS STUFF SO HARD</h1>');
@@ -9,4 +9,4 @@ $(document).ready(function() {
   // selecting the h2 nested within its list item.
 
         $("h2").remove().parent().parent();
-});
+      });
