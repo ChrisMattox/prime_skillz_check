d4098f0095998c02b3e347f982d23032a761ead8
Christopher Mattox <christophermattox@Christophers-MacBook-Pro.local>
2016-10-31 16:00:24 -0500

    Adding final tweaks

--- 9-jquery/scripts.js
+++ 9-jquery/scripts.js
@@ -4,7 +4,9 @@ $(document).ready(function() {
   function appendDom() {
         $('#section').append('<p>Page Loaded</p>');
 	// 2 - Change the text of the h1 element to something (anything!) else.
-
+        $('h1').append('<h1>WHY IS STUFF SO HARD</h1>');
   // 3 - Get rid of (delete) the useless unordered list (ul) by first
   // selecting the h2 nested within its list item.
-}
+
+        $("h2").remove().parent().parent();
+});
