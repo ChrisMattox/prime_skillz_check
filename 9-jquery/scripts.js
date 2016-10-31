5e2a2214e758e921bf41c98f2bcab8d43bbef339
Christopher Mattox <christophermattox@Christophers-MacBook-Pro.local>
2016-10-31 15:55:39 -0500

    Adding more saved stuff like git-it and jquery

--- 9-jquery/scripts.js
+++ 9-jquery/scripts.js
@@ -1,7 +1,10 @@
 	// 1 - After the DOM loads, add a paragraph tag that says "Page loaded"
 	// to the section element
-
+$(document).ready(function() {
+  function appendDom() {
+        $('#section').append('<p>Page Loaded</p>');
 	// 2 - Change the text of the h1 element to something (anything!) else.
 
   // 3 - Get rid of (delete) the useless unordered list (ul) by first
   // selecting the h2 nested within its list item.
+}
